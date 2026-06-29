39b9c3b2078d06e90bffa97d6f8a3ce27c35a41b
tribute1881-oss <tribute1881@gmail.com>
2026-06-29 13:57:01 +0500

    rename branding

---
+++ rename.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('public/index.html', 'utf8');
+html = html.replace('например: линк бот минерва', 'например: линк бот атаман');
+html = html.replace('link-rotator', 'Main Office');
+fs.writeFileSync('public/index.html', html);
+console.log('Готово!');
